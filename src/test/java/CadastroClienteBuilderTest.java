4d0555fba12bf8125a2defaf23d29149520d27f3
RobsonBSI <103039603+RobsonBSI@users.noreply.github.com>
2023-03-29 20:52:24 -0300

    atualização2

--- src/test/java/CadastroClienteBuilderTest.java
+++ src/test/java/CadastroClienteBuilderTest.java
@@ -16,4 +16,29 @@ class CadastroClienteBuilderTest {
             assertEquals("id inválida", e.getMessage());
         }
     }
+    @Test
+    void deveRetornarExcecaoParaAlunoSemNome() {
+        try {
+            CadastroClienteBuilder clienteBuilder = new CadastroClienteBuilder();
+            CadastroCliente cliente = clienteBuilder
+                    .setId(1)
+                    .setCep("999999999-99")
+                    .build();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Nome inválido", e.getMessage());
+        }
+    }
+    @Test
+    void deveRetornarAlunoValido() {
+        CadastroClienteBuilder clienteBuilder = new CadastroClienteBuilder();
+        CadastroCliente cliente= clienteBuilder
+                .setId(1)
+                .setNome("cliente 1")
+                .setCep("999999999-99")
+                .build();
+
+        assertNotNull(cliente);
+    }
+
 }
